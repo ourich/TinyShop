c2e0a891b6d3837a934c449309624b684dd63ad8
richat <richat@126.com>
2020-05-08 17:40:58 +0800

    全量油站

--- frontend/controllers/DefaultController.php
+++ frontend/controllers/DefaultController.php
@@ -99,9 +99,9 @@ class DefaultController extends BaseController
             'status' => 1,
           ];
           $count += 1;
-          if ($count >= 5) {
-              break;
-          }
+          // if ($count >= 5) {
+          //     break;
+          // }
         }
 
         //再执行批量插入
