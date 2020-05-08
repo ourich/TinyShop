0f0f73daf4e1263ca5e703aa8c3d41056ba73779
richat <richat@126.com>
2020-05-08 17:31:44 +0800

    test

--- frontend/controllers/DefaultController.php
+++ frontend/controllers/DefaultController.php
@@ -41,43 +41,24 @@ class DefaultController extends BaseController
         die();
         // return $this->render('index',[]);
     }
-    /**
-     * 重组显示
-     *
-     * @param $model
-     * @return mixed
-     */
-    public function regroupShow($model, $latitude, $longitude)
+    public function actionTest()
     {
-        // 是否可领取 
-        $other = OilStations::find()->select('gasAddress,gasAddressLatitude,gasAddressLongitude')->where(['gasId'=>$model['gasId']])->one();
-        $model['gasAddress'] = $other['gasAddress'];
-        $model['gasAddressLongitude'] = $other['gasAddressLongitude'];
-        $model['gasAddressLatitude'] = $other['gasAddressLatitude'];
-        $model['distance'] = $this->getDistance($latitude, $longitude, $model['gasAddressLatitude'], $model['gasAddressLongitude']);
-        $model['oilPriceList'] = ArrayHelper::index($model['oilPriceList'], 'oilNo');
-        $model['priceYfq'] = ArrayHelper::getValue($model['oilPriceList'], '92.priceYfq');
-        $model['priceOfficial'] = ArrayHelper::getValue($model['oilPriceList'], '92.priceOfficial');
-        $model['priceDiscount'] = $model['priceOfficial'] - $model['priceYfq'];
+        print_r('<pre>');
+        $mobile = '13098878085';
+        $gasId = 'JY000011413';
 
-        return $model;
-    }
+        $model = new OilStations();
+        $response = $model->find()->where(['gasId' => $gasId])->one();
 
-    //计算经纬度距离  km单位
-    public function getDistance($lat1, $lng1, $lat2, $lng2)
-    {
-        $earthRadius = 6367; //地区半径6367km
-        $lat1 = ($lat1 * pi()) / 180;
-        $lng1 = ($lng1 * pi()) / 180;
-        $lat2 = ($lat2 * pi()) / 180;
-        $lng2 = ($lng2 * pi()) / 180;
-        $calcLongitude      = $lng2 - $lng1;
-        $calcLatitude       = $lat2 - $lat1;
-        $stepOne            = pow(sin($calcLatitude / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($calcLongitude / 2), 2);
-        $stepTwo            = 2 * asin(min(1, sqrt($stepOne)));
-        $calculatedDistance = $earthRadius * $stepTwo;
-        return round($calculatedDistance, 1);
+
+
+
+        
+        print_r($response);
+        die();
+        // return $this->render('index',[]);
     }
+    
 
     /**
      * 更新油站数据
@@ -118,9 +99,9 @@ class DefaultController extends BaseController
             'status' => 1,
           ];
           $count += 1;
-          // if ($count >= 5) {
-          //     break;
-          // }
+          if ($count >= 5) {
+              break;
+          }
         }
 
         //再执行批量插入
