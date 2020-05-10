de2398c6418fffe3d8c73bd0fa732db188398f44
richat <richat@126.com>
2020-05-10 14:27:39 +0800

    加油订单表

--- backend/modules/common/views/oil-order/index.php
+++ backend/modules/common/views/oil-order/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Oil Orders';
+$this->title = '加油订单';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
@@ -31,51 +31,111 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
 
             'id',
-            'status',
-            'from',
-            'orderId',
-            'paySn',
-            'phone',
-            'orderTime',
-            'payTime',
-            'refundTime',
-            'gasName',
-            'province',
-            'city',
-            'county',
+            // 'status',
+            // 'from',
+            // 'orderId',
+            // 'paySn',
+            // 'phone',
+            [
+                'label' => '用户',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "订单号：" . $model->orderId . '<br>' .
+                        "支付号：" . $model->paySn . '<br>' .
+                        "手机：" . $model->phone . '<br>'  ;
+                },
+                'format' => 'raw',
+            ],
+            // 'orderTime',
+            // 'payTime',
+            // 'refundTime',
+            [
+                'label' => '订单时间',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "下单时间：" . Yii::$app->formatter->asDatetime($model->orderTime) . '<br>' .
+                        "付款时间：" . Yii::$app->formatter->asDatetime($model->payTime) . '<br>' .
+                        "退款时间：" . Yii::$app->formatter->asDatetime($model->refundTime) . '<br>' ;
+                },
+                'format' => 'raw',
+            ],
+            // 'province',
+            // 'city',
+            // 'county',
+            [
+                'label' => '位置',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "省份：" . $model->province . '<br>' .
+                        "城市：" . $model->city . '<br>' .
+                        "区县：" . $model->county . '<br>'  ;
+                },
+                'format' => 'raw',
+            ],
             //'gunNo',
-            'oilNo',
-            'amountPay',
-            'amountGun',
-            'amountDiscounts',
-            'orderStatusName',
-            'couponMoney',
+            // 'gasName',
+            // 'oilNo',
+            [
+                'label' => '油站',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "油站：" . $model->gasName . '<br>' .
+                        "油号：" . $model->oilNo . '<br>' .
+                        "升数：" . $model->litre . '<br>'  ;
+                },
+                'format' => 'raw',
+            ],
+            // 'amountPay',
+            // 'amountGun',
+            // 'amountDiscounts',
+            [
+                'label' => '金额',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "总额：" . $model->amountGun . '<br>' .
+                        "实付：" . $model->amountPay . '<br>' .
+                        "优惠：" . $model->amountDiscounts . '<br>'  ;
+                },
+                'format' => 'raw',
+            ],
+            [
+                'label' => '状态',
+                'filter' => false, //不显示搜索框
+                'value' => function ($model) {
+                    return "订单状态：" . $model->orderStatusName . '<br>' .
+                        "支付方式：" . $model->payType . '<br>' .
+                        "实际单价：" . $model->priceUnit . '<br>'  ;
+                },
+                'format' => 'raw',
+            ],
+            // 'orderStatusName',
+            // 'couponMoney',
             //'couponId',
             //'couponCode',
-            'litre',
-            'payType',
-            'priceUnit',
-            'priceOfficial',
-            'priceGun',
+            // 'litre',
+            // 'payType',
+            // 'priceUnit',
+            // 'priceOfficial',
+            // 'priceGun',
             //'orderSource',
             //'qrCode4PetroChina',
             //'created_at',
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'header' => '操作',
-                'template' => '{edit} {status} {delete}',
-                'buttons' => [
-                'edit' => function($url, $model, $key){
-                        return Html::edit(['edit', 'id' => $model->id]);
-                },
-               'status' => function($url, $model, $key){
-                        return Html::status($model['status']);
-                  },
-                'delete' => function($url, $model, $key){
-                        return Html::delete(['delete', 'id' => $model->id]);
-                },
-                ]
-            ]
+            // [
+            //     'class' => 'yii\grid\ActionColumn',
+            //     'header' => '操作',
+            //     'template' => '{edit} {status} {delete}',
+            //     'buttons' => [
+            //     'edit' => function($url, $model, $key){
+            //             return Html::edit(['edit', 'id' => $model->id]);
+            //     },
+            //    'status' => function($url, $model, $key){
+            //             return Html::status($model['status']);
+            //       },
+            //     'delete' => function($url, $model, $key){
+            //             return Html::delete(['delete', 'id' => $model->id]);
+            //     },
+            //     ]
+            // ]
     ]
     ]); ?>
             </div>
