d0528668f3ce7dd7533e3c1389693bfc776f4fbc
richat <richat@126.com>
2020-05-29 11:51:08 +0800

    扣减优惠金

--- frontend/controllers/DefaultController.php
+++ frontend/controllers/DefaultController.php
@@ -72,7 +72,7 @@ class DefaultController extends BaseController
                    ->execute();
 
               //更新会员优惠金【暂不开启】
-              foreach ($$data as $value) {
+              foreach ($data as $value) {
                 Yii::$app->tinyShopService->order->jiChaOil($value);
               }
             }
