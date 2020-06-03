73a4a0f9db1d0694459c9b933d4194147688b49d
richat <richat@126.com>
2020-06-03 17:37:30 +0800

    油站全量数据

--- frontend/controllers/DefaultController.php
+++ frontend/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace addons\TinyShop\frontend\controllers;
 
 use Yii;
 use common\controllers\AddonsController;
+use addons\TinyShop\common\models\gas\GasStations;
 
 /**
  * 默认控制器
@@ -22,8 +23,9 @@ class DefaultController extends BaseController
     {
         //测试入口
         $mobile = '13098878085';
-        $test = Yii::$app->tinyShopService->czb->login($mobile);
-        p($test);
+        // $test = Yii::$app->tinyShopService->czb->login($mobile);
+        // $response = Yii::$app->tinyShopService->czb->queryGasInfoListOilNoNew();
+        p($response);
         die();
 
 
@@ -32,5 +34,91 @@ class DefaultController extends BaseController
         return $this->render('index',[
 
         ]);
+    }
+
+    /**
+     * 同步车主邦订单
+     * @return [type] [description]
+     */
+    public function actionOrder()
+    {
+        $response = Yii::$app->tinyShopService->czb->platformOrderInfoV2();
+        
+        if ($response['code'] == 200) {
+            $result = $response['result'];
+            foreach ($result as $value) {
+                if (OilOrder::find()->where(['orderId' => $value['orderId']])->one()) {
+                    continue; //如果存在，则跳过
+                }
+                $value['created_at'] = time();
+                $data[] = $value;
+            }
+            //再执行批量插入
+            if (isset($data)) 
+            {
+              Yii::$app->db->createCommand()
+                   ->batchInsert(OilOrder::tableName(),['orderId','paySn','phone','orderTime','payTime','refundTime','gasName','province','city','county','gunNo','oilNo','amountPay','amountGun','amountDiscounts','orderStatusName','couponMoney','couponId','couponCode','litre','payType','priceUnit','priceOfficial','priceGun','orderSource','qrCode4PetroChina','gasId','created_at'],
+                   $data)
+                   ->execute();
+
+              //更新会员优惠金【暂不开启】
+              foreach ($data as $value) {
+                // Yii::$app->tinyShopService->order->jiChaOil($value);
+              }
+            }
+        }
+
+        die(count($data));
+    }
+
+    /**
+     * 更新加油站列表
+     * @return [type] [description]
+     */
+    public function actionUpstations()
+    {
+        $response = Yii::$app->tinyShopService->czb->queryGasInfoListOilNoNew();
+        if ($response['code'] != 200) {
+            return;
+        }
+        $model = new GasStations();
+        $result = $response['result'];
+        foreach($result as $v)
+        {
+          if ($model->find()->where(['gasId' => $v['gasId']])->one()) {
+              continue; //如果存在，则跳过
+          }
+
+          $data[] = [
+            'gasId' => $v['gasId'],
+            'gasName' => $v['gasName'],
+            'gasType' => $v['gasType'],
+            'gasLogoBig' => $v['gasLogoBig'],
+            'gasLogoSmall' => $v['gasLogoSmall'],
+            'gasAddress' => $v['gasAddress'],
+            'gasAddressLongitude' => $v['gasAddressLongitude'],
+            'gasAddressLatitude' => $v['gasAddressLatitude'],
+            'provinceCode' => $v['provinceCode'],
+            'cityCode' => $v['cityCode'],
+            'countyCode' => $v['countyCode'],
+            'provinceName' => $v['provinceName'],
+            'cityName' => $v['cityName'],
+            'countyName' => $v['countyName'],
+            'isInvoice' => $v['isInvoice'],
+            'companyId' => $v['companyId'],
+            'created_at' => time(),
+            'channelId' => 0,
+            'status' => 1,
+          ];
+        }
+        //再执行批量插入
+        if (isset($data)) 
+        {
+          Yii::$app->db->createCommand()
+               ->batchInsert(GasStations::tableName(),['gasId','gasName','gasType','gasLogoBig','gasLogoSmall','gasAddress','gasAddressLongitude','gasAddressLatitude','provinceCode','cityCode','countyCode','provinceName','cityName','countyName','isInvoice','companyId','created_at','channelId','status'],
+               $data)
+               ->execute();
+        }
+        die($count);
     }
 }
