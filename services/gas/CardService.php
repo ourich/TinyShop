b23e62fc40d5d0523ad17d858c6d4efe7cbcd26f
richat <richat@126.com>
2020-06-04 17:31:46 +0800

    更新存在的卡片，创建不存在的

--- services/gas/CardService.php
+++ services/gas/CardService.php
@@ -7,6 +7,8 @@ use common\components\Service;
 use addons\TinyShop\common\models\forms\GasCardForm;
 use yii\web\NotFoundHttpException;
 use common\helpers\StringHelper;
+use common\helpers\ArrayHelper;
+use common\enums\StatusEnum;
 
 /**
  * Class TransmitService
@@ -27,7 +29,22 @@ class CardService extends Service
         $rows = [];
         $min = $model->give_begin;
         $max = $model->give_end;
+        //已存在的卡片(包含边界)
+        $cards =  GasCardForm::find()
+            ->where(['status' => StatusEnum::ENABLED])
+            ->andWhere(['between', 'cardNo', $min, $max])
+            ->orderBy('id desc')
+            ->asArray()
+            ->all();
+        $cardids = ArrayHelper::getColumn($cards, 'id');
+        $ids=implode(',',$cardids);
+        Yii::$app->db->createCommand()->update(GasCardForm::tableName(), ['member_id' => $model->give_to], "id in ($ids)")->execute();
+        //更新存在的卡片，创建不存在的
+        $cardNos = ArrayHelper::index($cards, 'cardNo');
         for ($i = $min; $i <= $max; $i++) {
+            if (ArrayHelper::keyExists($i, $cardNos, false)) {
+                continue;
+            }
             $rows[] = [
                 'cardNo' => $i,
                 'member_id' => $model->give_to,
@@ -36,6 +53,8 @@ class CardService extends Service
                 'status' => 1,
             ];
         }
+        // p($rows);
+        // die();
 
         $field = ['cardNo', 'member_id', 'code', 'created_at', 'status'];
         !empty($rows) && Yii::$app->db->createCommand()->batchInsert(GasCardForm::tableName(), $field, $rows)->execute();
