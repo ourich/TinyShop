9c2006042b894d48c2d17d2ad166bb73edb94d15
richat <richat@126.com>
2020-07-21 12:46:35 +0800

    系统默认返回

--- api/modules/v1/controllers/marketing/OilController.php
+++ api/modules/v1/controllers/marketing/OilController.php
@@ -61,7 +61,7 @@ class OilController extends OnAuthController
         $outinfo = $xiaoju->jiami($queryData);
         // Yii::error('-------------用户信息校验------'.print_r($outinfo, 1));
         // return $outinfo;
-        return ResultHelper::xiaojuJson(200, '请求成功', $outinfo['data'], $outinfo['appKey'], $outinfo['sig']);
+        return ResultHelper::json(200, '请求成功', $outinfo['data']);
 
         // {
         //   "code": 200,
