47077a385da480d265ff2e221e65c3803e3cb7d2
richat <richat@126.com>
2020-07-21 11:25:02 +0800

    原始sig

--- api/modules/v1/controllers/marketing/OilController.php
+++ api/modules/v1/controllers/marketing/OilController.php
@@ -51,10 +51,10 @@ class OilController extends OnAuthController
     public function actionNotifyCheckUserInfo()
     {
         $data = Yii::$app->request->post();
-        $mobile = $data['mobile'];
-        $outUserId = $data['outUserId'];
+        $sig = $data['sig'];
+        $outdata = $data['data'];
         Yii::error('-------------用户信息11校验------'.print_r($data, 1));
-        return ResultHelper::json(200, '解密：'.$mobile);
+        return ResultHelper::json(200, '解密：'.$sig);
     }
 
     /**
